f68fdd6caa8b8b739c60f8ee208b6a39f94ba654
Rahdansuwa24 <dewaruci393@gmail.com>
2026-06-24 10:48:30 +0700

    menambahkan perubahan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import SimulatePage from "./pages/SimulatePage";
 import DicomWorklistPage from './pages/DicomWorklistPage'
 import DicomViewerPage from './pages/DicomViewerPage'
 import PatientViewerPage from './pages/patientViewer'
-import WorklistPage from './pages/worklistPage'
+import RadiologyWorklistPage from './pages/worklist/RadiologyWorklistPage'
+import PolyclinicWorklistPage from './pages/worklist/PolyclinicWorklistPage'
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
         <Route path="/dicom-worklist" element={<DicomWorklistPage />} />
         <Route path="/dicom-viewer" element={<DicomViewerPage />} />
         <Route path="/patient-viewer" element={<PatientViewerPage />} />
-        <Route path="/worklist" element={<WorklistPage />} />
+        <Route path="/worklist" element={<RadiologyWorklistPage />} />
+        <Route path="/worklist-radiology" element={<RadiologyWorklistPage />} />
+        <Route path="/worklist-polyclinic" element={<PolyclinicWorklistPage />} />
       </Routes>
     </BrowserRouter>
   );
